92058639403d3efd733cabacfbe57bc316d6a9dd
Matthew McMillan <matthew.mcmillan@gmail.com>
2020-08-01 21:12:47 -0500

    Fix for reading negative temperatures. Allow the use of multiple chip id's. Add functions so temperature can be returned in celsius or fahrenheit.

--- temp.py
+++ temp.py
@@ -1,5 +1,16 @@
 import LM75
 
-sensor = LM75.LM75()
+sensor1 = LM75.LM75()
+sensor1.i2c_address = 0x48
 
-print sensor.getTemp()
+sensor2 = LM75.LM75()
+sensor2.i2c_address = 0x49
+
+
+print("temp_c:", sensor1.getCelsius())
+print("temp_f:", sensor1.getFahrenheit())
+print("----------------------------------")
+
+
+#print("temp_c:", sensor2.getCelsius())
+#print("----------------------------------")
